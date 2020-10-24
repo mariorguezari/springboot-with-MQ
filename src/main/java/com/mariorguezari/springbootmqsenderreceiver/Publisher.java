bfd40ed3bfe2ca57e1d5035fc6f199b116b467d2
Rodriguez Perez <juan.rodriguezp@atos.net>
2020-10-24 03:20:20 +0200

    Fix Publisher

--- src/main/java/com/mariorguezari/springbootmqsenderreceiver/Publisher.java
+++ src/main/java/com/mariorguezari/springbootmqsenderreceiver/Publisher.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
- 
+
 @RestController
 @RequestMapping(value = "/api")
 public class Publisher {
