8dff71d092eb64538842cb65cf9eb9b66bb665a3
Rodriguez Perez <juan.rodriguezp@atos.net>
2020-10-24 02:36:22 +0200

    Create project from initializer

---
+++ src/test/java/com/mariorguezari/springbootmqsenderreceiver/SpringbootMqSenderReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariorguezari.springbootmqsenderreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMqSenderReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
